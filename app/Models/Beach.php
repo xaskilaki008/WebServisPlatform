af20180aecc3f93ed45d46f3de8616ffc538e416
xaskilaki008 <fff007fqp@gmail.com>
2026-03-23 16:54:43 +0300

    добавил базу данных

---
+++ app/Models/Beach.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Beach extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'number',
+        'name',
+        'latitude',
+        'longitude',
+        'wave_level',
+    ];
+
+    protected $casts = [
+        'number' => 'integer',
+        'latitude' => 'float',
+        'longitude' => 'float',
+        'wave_level' => 'integer',
+    ];
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,9 @@
-<?php
-
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+﻿<?php
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
